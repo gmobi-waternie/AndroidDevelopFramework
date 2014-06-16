1d0cc518cddb5abd786683a127bdb5e75c9bf0f6
charon.chui@gmail.com <CharonChui@web30.bbn.com.cn>
2014-06-16 20:59:27 +0800

    Modify SimpleAsyncTask

--- AndroidDevelopFramework/src/com/charon/framework/SimpleAsyncTask.java
+++ AndroidDevelopFramework/src/com/charon/framework/SimpleAsyncTask.java
@@ -6,14 +6,9 @@ import android.os.Handler;
 /**
  * 利用Handler实现的简单异步任务
  */
-@SuppressLint("HandlerLeak")
 public abstract class SimpleAsyncTask {
 
-	private Handler handler = new Handler() {
-		public void handleMessage(android.os.Message msg) {
-			onPostExecute();
-		};
-	};
+	private Handler handler = new Handler();
 
 	/**
 	 * 后台任务执行之前 提示用户的界面操作.
@@ -23,19 +18,25 @@ public abstract class SimpleAsyncTask {
 	/**
 	 * 后台任务执行之后 更新界面的操作.
 	 */
-	public abstract void onPostExecute();
+	public abstract void onPostExecute(boolean success);
 
 	/**
 	 * 在后台执行的一个耗时的操作.
 	 */
-	public abstract void doInBackground();
+	public abstract boolean doInBackground();
 
 	public void execute() {
 		onPreExecute();
 		new Thread() {
 			public void run() {
-				doInBackground();
-				handler.sendEmptyMessage(0);
+				final boolean success = doInBackground();
+				handler.post(new Runnable() {
+					
+					@Override
+					public void run() {
+						onPostExecute(success);	
+					}
+				});
 			};
 		}.start();
 	}
