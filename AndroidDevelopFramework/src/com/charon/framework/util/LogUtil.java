02323cd51cf5f818bfe1bba7d7295caff3d6bc78
charon.chui@gmail.com <charon.chui@gmail.com>
2014-06-30 00:42:36 +0800

    Close log print

--- AndroidDevelopFramework/src/com/charon/framework/util/LogUtil.java
+++ AndroidDevelopFramework/src/com/charon/framework/util/LogUtil.java
@@ -1,3 +1,4 @@
+
 package com.charon.framework.util;
 
 import android.util.Log;
@@ -6,41 +7,44 @@ import android.util.Log;
  * 自定义Log打印工具类
  */
 public class LogUtil {
-	private static final int LOG_LEVEL = 6;
+    /**
+     * If print log here.
+     */
+    private static int LOG_LEVEL = 1;
 
-	private static final int VERBOSE = 5;
-	private static final int DEBUG = 4;
-	private static final int INFO = 3;
-	private static final int WARN = 2;
-	private static final int ERROR = 1;
+    private static final int VERBOSE = 5;
+    private static final int DEBUG = 4;
+    private static final int INFO = 3;
+    private static final int WARN = 2;
+    private static final int ERROR = 1;
 
-	public static void v(String tag, String msg) {
-		if (LOG_LEVEL > VERBOSE) {
-			Log.v(tag, msg);
-		}
-	}
+    public static void v(String tag, String msg) {
+        if (LOG_LEVEL > VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
 
-	public static void d(String tag, String msg) {
-		if (LOG_LEVEL > DEBUG) {
-			Log.d(tag, msg);
-		}
-	}
+    public static void d(String tag, String msg) {
+        if (LOG_LEVEL > DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
 
-	public static void i(String tag, String msg) {
-		if (LOG_LEVEL > INFO) {
-			Log.i(tag, msg);
-		}
-	}
+    public static void i(String tag, String msg) {
+        if (LOG_LEVEL > INFO) {
+            Log.i(tag, msg);
+        }
+    }
 
-	public static void w(String tag, String msg) {
-		if (LOG_LEVEL > WARN) {
-			Log.w(tag, msg);
-		}
-	}
+    public static void w(String tag, String msg) {
+        if (LOG_LEVEL > WARN) {
+            Log.w(tag, msg);
+        }
+    }
 
-	public static void e(String tag, String msg) {
-		if (LOG_LEVEL > ERROR) {
-			Log.e(tag, msg);
-		}
-	}
+    public static void e(String tag, String msg) {
+        if (LOG_LEVEL > ERROR) {
+            Log.e(tag, msg);
+        }
+    }
 }
