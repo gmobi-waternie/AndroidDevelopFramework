a7571c39c5d39f94fee9800b7964ee15f8f34ebe
charon.chui@gmail.com <charon.chui@gmail.com>
2014-07-22 19:40:47 +0800

    Add IntentUtil

---
+++ AndroidDevelopFramework/src/com/charon/framework/util/IntentUtil.java
@@ -0,0 +1,18 @@
+
+package com.charon.framework.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+public class IntentUtil {
+    public static boolean isIntentAvailable(Context context, Intent intent) {
+        PackageManager packageManager = context.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+                PackageManager.GET_ACTIVITIES);
+        return list.size() > 0;
+    }
+}
