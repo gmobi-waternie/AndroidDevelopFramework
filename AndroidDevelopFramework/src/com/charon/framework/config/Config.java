04ff131c76fd3f35525b023c54ffd362b49d8833
charon.chui@gmail.com <charon.chui@gmail.com>
2014-11-05 17:29:04 +0800

    Add Config and LogUtil

---
+++ AndroidDevelopFramework/src/com/charon/framework/config/Config.java
@@ -0,0 +1,14 @@
+package com.charon.framework.config;
+
+/**
+ * Config file.
+ * 
+ * @author CharonChui
+ * 
+ */
+public interface Config {
+    /**
+     * If is in the debug mode.
+     */
+    boolean DEBUG = true;
+}
